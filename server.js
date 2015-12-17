cfa2a973f88dc66c78725019ccf792077e1e7893
Nagesh Kathare <nkathare@gmail.com>
2015-12-17 19:07:08 +0000

    v2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
     connection.query('SELECT * from gumball', function(err, rows, fields) {
       if (!err)
         // console.log('The solution is: ', rows);
-        res.send("Hello AWS MySQL!<br/>The Count of gumballs is: "+rows[0].count_gumballs+"<br/>The Gumball Machine Model is:  "+rows[0].modelNumber+"</br>The Gumball Machine Serial # is: "+ rows[0].serialNumber);
+        res.send("Hello AWS MySQL!<br/>The Count of gumballs is: "+rows[0].count_gumballs+"<br/>The Gumball Machine Model is:  "+rows[0].model_number+"</br>The Gumball Machine Serial # is: "+ rows[0].serial_number);
         else
         console.log('Error while performing Query.');
     
