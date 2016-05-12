94d84b491e9706a05096851fe4640771014560d0
Nagesh Kathare <nageshsk@users.noreply.github.com>
2016-05-11 20:07:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ app.get('/', function (req, res) {
 
     connection.query('SELECT * from gumball', function(err, rows, fields) {
       if (!err)
-        // console.log('The solution is: ', rows);
         res.send("Hello AWS MySQL!<br/>The Count of gumballs is: "+rows[0].count_gumballs+"<br/>The Gumball Machine Model is:  "+rows[0].model_number+"</br>The Gumball Machine Serial # is: "+ rows[0].serial_number);
         else
         console.log('Error while performing Query.');
     
     });
     
-      //res.send('Hello World!');
 });
 
 app.listen( process.env.PORT );
